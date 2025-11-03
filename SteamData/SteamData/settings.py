d4bc39926aa9e98b99c28429e83fa297517aceac
Paige <paigeredlin@mail.weber.edu>
2025-11-03 13:19:16 -0700

    Alter installed apps paths

--- SteamData/SteamData/settings.py
+++ SteamData/SteamData/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'MainApp',
-    'GamesApp',
+    'SteamData.MainApp',
+    'SteamData.GamesApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
