3d6ed2e56a0f71de6f4d3466b41b0ebb42b0f809
Paige <paigeredlin@mail.weber.edu>
2025-11-08 14:59:22 -0700

    Trusted Origins

--- SteamData/SteamData/settings.py
+++ SteamData/SteamData/settings.py
@@ -144,3 +144,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://capstonewebapplication-production.up.railway.app",
+]
