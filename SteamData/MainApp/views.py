5258769adca21da5a2aeacb27149a24a42e8f4b4
Paige <paigeredlin@mail.weber.edu>
2025-11-20 20:55:54 -0700

    Design improvements

--- SteamData/MainApp/views.py
+++ SteamData/MainApp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from GamesApp.models import App
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    featured_apps = App.objects.all()[:5]
+    context = {
+        'featured_apps' : featured_apps
+    }
+    return render(request, 'home.html', context)
 
 def documentation(request):
     return render(request, 'documentation.html')
